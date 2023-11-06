dca1af382cb82ebb84e926a5be0e1a0950cd112f
kongxiaojun <kongxiaojun@58.com>
2023-11-06 16:41:11 +0800

    fix: 新增StickyView组件30%

--- packages/taro-components-mpharmony/src/components/sticky/sticky.tsx
+++ packages/taro-components-mpharmony/src/components/sticky/sticky.tsx
@@ -1,4 +1,5 @@
 import { StandardProps } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 import React, { Component } from 'react'
 
 export interface StickyProps extends StandardProps {
@@ -15,26 +16,93 @@ interface State {
   containerHeight?: number
 }
 
-export class StickyView extends Component<StickyProps, State> {
+export class Sticky extends Component<StickyProps, State> {
 
-  componentDidMount () {
-    this.setState({
-      attached: true,
+  getRect = (selector) => {
+    return new Promise((resolve, reject) => {
+      Taro.createSelectorQuery()
+        .select(selector)
+        .boundingClientRect(rect => {
+          if (rect) {
+            resolve(rect)
+          } else {
+            reject(new Error(`can not find selector: ${selector}`))
+          }
+        }).exec()
     })
   }
 
-  // shouldComponentUpdate(nextProps: Readonly<StickyProps>, nextState: Readonly<State>, nextContext: any): boolean {
-  //   if (nextProps.disabled != this.props.disabled) {
-  //
-  //   }
-  //
-  //   return true
-  // }
+  getAllRects = (selector) => {
+    return new Promise((resolve, reject) => {
+      Taro.createSelectorQuery()
+        .selectAll(selector)
+        .boundingClientRect(rects => {
+          if (rects) {
+            if (Array.isArray(rects) && rects.length > 0) {
+              resolve(rects)
+            } else {
+              resolve([rects])
+            }
+          } else {
+            reject(new Error(`can not find selector: ${selector}`))
+          }
+        }).exec()
+    })
+  }
+
+  componentDidMount () {
+    this.setState({
+      attached: true
+    })
+
+    if (!this.props.disabled) {
+      this.initObserver()
+    }
+  }
+
+  shouldComponentUpdate (nextProps: Readonly<StickyProps>, _nextState: Readonly<State>, _nextContext: any): boolean {
+    if (nextProps.disabled !== this.props.disabled) {
+      nextProps.disabled ? this.disconnectObserver('') : this.initObserver()
+    }
+    if (nextProps.container !== this.props.container) {
+      if (typeof nextProps.container === 'function' && this.state.height) {
+        this.observerContainer()
+      }
+    }
+    if (typeof nextProps.offsetTop === 'number' && this.state.attached) {
+      this.initObserver()
+    }
+    return true
+  }
 
   componentWillUnmount () {
     this.setState({
-      attached: false,
+      attached: false
     })
+    this.disconnectObserver('')
+  }
+
+  initObserver = () => {
+    this.disconnectObserver(undefined)
+    this.getRect(this).then((rect: any) => {
+      this.setState({
+        height: rect.height
+      })
+      this.observerContent()
+      this.observerContainer()
+    })
+  }
+
+  disconnectObserver = (_observerName) => {
+
+  }
+
+  observerContent = () => {
+
+  }
+
+  observerContainer = () => {
+
   }
 
   render () {
@@ -42,92 +110,4 @@ export class StickyView extends Component<StickyProps, State> {
       {this.props.children}
     </div>
   }
-
-
 }
-// = ({ children, offsetTop = 0, zIndex = 0, disabled = false, container = null }) => {
-//
-//   useEffect(() => {
-//     if (!attached) return
-//     disabled ? disconnectObserver('') : initObserver()
-//   }, [disabled])
-//
-//   useEffect(() => {
-//     if (typeof container !== 'function' || !height) return
-//     observerContainer()
-//   }, [container])
-//
-//   useEffect(() => {
-//     if (typeof offsetTop !== 'number' || !attached) return
-//     initObserver()
-//   }, [offsetTop])
-//
-//   useEffect(() => {
-//     setAttached(true)
-//     if (!disabled) initObserver()
-//     return function cleanup() {
-//       setAttached(false)
-//       disconnectObserver('')
-//     }
-//   }, [])
-//
-//   const getContainerRect = () => {
-//   }
-//
-//   const getRect = (selector) => {
-//     return new Promise((resolve, reject) => {
-//       Taro.createSelectorQuery()
-//         .select(selector)
-//         .boundingClientRect(rect => {
-//           if (rect) {
-//             resolve(rect)
-//           } else {
-//             reject(new Error(`can not find selector: ${selector}`))
-//           }
-//         }).exec()
-//     })
-//   }
-//
-//   const getAllRects = (selector) => {
-//     return new Promise((resolve, reject) => {
-//       Taro.createSelectorQuery()
-//         .selectAll(selector)
-//         .boundingClientRect(rects => {
-//           if (rects) {
-//             if (Array.isArray(rects) && rects.length > 0) {
-//               resolve(rects)
-//             } else {
-//               resolve([rects])
-//             }
-//           } else {
-//             reject(new Error(`can not find selector: ${selector}`))
-//           }
-//         }).exec()
-//     })
-//   }
-//
-//   const initObserver = () => {
-//     disconnectObserver(undefined)
-//     getRect(this).then((rect: any) => {
-//       setHeight(rect.height)
-//       observerContent()
-//       observerContainer()
-//     })
-//   }
-//
-//   const disconnectObserver = (observerName) => {
-//
-//   }
-//
-//   const observerContent = () => {
-//
-//   }
-//
-//   const observerContainer = () => {
-//
-//   }
-//
-//   return <div>
-//     {children}
-//   </div>
-// }
